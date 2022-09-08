15e335a2fc92e27aa0d134bd38f9e4515e9a8254
Anthony Rodriguez <rdz.tony1@gmail.com>
2022-09-08 00:44:19 -0500

    commit tree e0e4a57ad9fa1105499780087853ec9bca35ee5f

--- src/index.js
+++ src/index.js
@@ -13,3 +13,95 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+const divToyCollection = document.querySelector('#toy-collection');
+
+const form = document.querySelector('.add-toy-form');
+form.addEventListener('submit', handleSubmit)
+
+function handleSubmit(e){
+  e.preventDefault();
+
+  const toyObject = {
+      name: e.target.input1.value,
+      image: e.target.input2.value,
+      likes: 0
+  }
+
+ // console.log(e.target.input2.value)
+  // const toyObject = {
+  // console.log(e.target.input[0])
+
+  // }
+    postToyFromSubmit(toyObject)
+}
+
+function postToyFromSubmit(toyObject){
+  fetch('http://localhost:3000/toys', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(toyObject)
+  })
+  .then(resp=>resp.JSON)
+  //.then(data=>console.log(data))
+
+};
+
+
+function patchLikes(toyObject){
+  fetch(`http://localhost:3000/toys/${toyObject.id}`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(toyObject)
+  })
+  .then(resp=>resp.json())
+  .then(data=>console.log(data))
+};
+
+function loadAnimals (){
+fetch('http://localhost:3000/toys')
+.then(resp=>resp.json())
+.then(data=>{
+  data.forEach(element=>{
+    const div = document.createElement('div');
+    div.className='card'
+    const h2 = document.createElement('h2');
+    const img = document.createElement('img');
+    let pTag = document.createElement('p');
+    const bttn = document.createElement('button');
+    
+    h2.innerText = element.name;
+    img.src = element.image;
+    img.className = 'toy-avatar'
+    pTag.innerText = `${element.likes} Likes`;
+    bttn.className = 'like-btn';
+    bttn.setAttribute('id', `${element.id}`);
+    bttn.innerText = 'Like ❤️'
+
+    divToyCollection.appendChild(div);
+    div.appendChild(h2);
+    div.appendChild(img);
+    div.appendChild(pTag);
+    div.appendChild(bttn);
+
+
+    bttn.addEventListener('click', ()=>{
+      element.likes+=1
+      pTag.innerText=`${element.likes} Likes`
+
+    patchLikes(element);
+      
+
+    }
+    )})})}
+
+function start(){
+  loadAnimals();
+ 
+}
+
+start();
